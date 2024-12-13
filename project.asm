ff1296773a9e6784f461c0b01d44c1ce539675d5
Hussein <shsmohmed4@gmail.com>
2024-12-13 13:39:52 +0200

    Implement paddle drawing and movement logic for game functionality

---
+++ project.asm
@@ -0,0 +1,248 @@
+
+
+.model small    
+.data
+    paddle_one_x    dw 280    ; X-coordinate for the right-bottom paddle
+    paddle_one_y    dw 190    ; Y-coordinate for the right-bottom paddle
+    paddle_two_x    dw 10     ; X-coordinate for the left-bottom paddles
+    paddle_two_y    dw 190    ; Y-coordinate for the left-bottom paddles
+    paddle_width    dw 30     ; Paddle width
+    paddle_height   dw 5      ; Paddle height
+    paddle_velocity dw 5      ;paddle move speed
+
+.stack 100h
+.code
+main proc far
+
+                             mov  ax,@data
+                             mov  ds,ax
+                             call CLEAR_SCREEN
+    main_loop:               
+                             call move_paddles
+                             call draw_paddles
+                             jmp  main_loop
+
+
+                             mov  ah, 0
+                             int  16h
+                             mov  ah,4ch
+                             int  21h
+main endp
+
+CLEAR_SCREEN PROC far                                         ;clear the screen by restarting the video mode
+	
+                             MOV  AH,00h                      ;set the configuration to video mode
+                             MOV  AL,13h                      ;choose the video mode
+                             INT  10h                         ;execute the configuration
+		
+                             MOV  AH,0Bh                      ;set the configuration
+                             MOV  BH,00h                      ;to the background color
+                             MOV  BL,00h                      ;choose black as background color
+                             INT  10h                         ;execute the configuration
+			
+                             RET
+			
+CLEAR_SCREEN ENDP
+
+
+draw_paddles proc far
+
+                             mov  cx , paddle_one_x
+                             mov  dx , paddle_one_y
+    paddle1_horizontal:      
+                             mov  ah, 0ch                     ; pixel color interrupt
+                             mov  al, 0Fh                     ;pixel color white
+                             mov  bh,0h
+                             int  10h                         ;call interrupt
+                             inc  cx                          ; move to left
+                             mov  ax , cx
+                             sub  ax , paddle_one_x
+                             cmp  ax , paddle_width
+                             jl   paddle1_horizontal
+
+                             mov  cx , paddle_one_x
+                             inc  dx
+
+                             mov  ax , dx
+                             sub  ax , paddle_one_y
+                             cmp  ax , paddle_height
+                             jl   paddle1_horizontal
+
+                             mov  cx , paddle_two_x
+                             mov  dx , paddle_two_y
+
+    paddle2_horizontal:      
+                             mov  ah, 0ch                     ; pixel color interrupt
+                             mov  al, 0Fh                     ;pixel color white
+                             mov  bh,0h
+                             int  10h                         ;call interrupt
+                             inc  cx                          ; move to left
+                             mov  ax , cx
+                             sub  ax , paddle_two_x
+                             cmp  ax , paddle_width
+                             jl   paddle2_horizontal
+
+                             mov  cx , paddle_two_x
+                             inc  dx
+
+                             mov  ax , dx
+                             sub  ax , paddle_two_y
+                             cmp  ax , paddle_height
+                             jl   paddle2_horizontal
+
+
+
+                             ret
+
+draw_paddles endp
+
+
+move_paddles proc far
+
+    ;check if key pressed
+
+                             mov  ah , 01h
+                             int  16h
+                             jz   exit_paddle_movement
+    ;check which key is pressed
+
+                             mov  ah , 00h
+                             int  16h                         ; al = ASCII , ah = scan code
+
+    ;if up arrow move up
+
+                             cmp  ah , 4dh
+                             je   move_paddle1_right
+    ;if down arrow move down
+                             cmp  ah , 4bh
+                             je   move_paddle1_left
+
+                             jmp  check_paddle2_movement
+
+    move_paddle1_right:      
+                             call clear_paddle1
+                             mov  ax , paddle_velocity
+    ; Boundary check for the right edge of the screen
+                             mov  bx, paddle_one_x
+                             add  bx, ax
+                             add  bx, paddle_width
+                             cmp  bx, 320                     ; Assuming screen width is 320 pixels
+                             jg   paddle1_skip_right          ; If beyond right boundary, skip update
+
+                             add  paddle_one_x , ax
+    paddle1_skip_right:      
+                             jmp  check_paddle2_movement
+
+
+    move_paddle1_left:       
+                             call clear_paddle1
+                             mov  ax , paddle_velocity
+    ; Boundary check for the left edge of the screen
+                             mov  bx, paddle_one_x
+                             sub  bx, ax
+                             cmp  bx, 0                       ; Ensure it doesn't go below 0
+                             jl   paddle1_skip_left           ; If beyond left boundary, skip update
+                             sub  paddle_one_x , ax
+    paddle1_skip_left:       
+                             jmp  check_paddle2_movement
+
+    ; paddle2_movement
+    check_paddle2_movement:  
+
+    ; D | d to right
+                             cmp  al , 64h
+                             je   move_paddle2_right
+                             cmp  al , 44h
+                             je   move_paddle2_right
+    ; A | a to left
+                             cmp  al , 61h
+                             je   move_paddle2_left
+                             cmp  al , 41h
+                             je   move_paddle2_left
+
+                             jmp  exit_paddle_movement
+    move_paddle2_right:      
+                             call clear_paddle2
+                             mov  ax , paddle_velocity
+    ; Boundary check for the right edge of the screen
+                             mov  bx, paddle_two_x
+                             add  bx, ax
+                             add  bx, paddle_width
+                             cmp  bx, 320                     ; Assuming screen width is 320 pixels
+                             jg   paddle2_skip_right          ; If beyond right boundary, skip update
+
+                             add  paddle_two_x , ax
+    paddle2_skip_right:      
+                             jmp  exit_paddle_movement
+
+
+    move_paddle2_left:       
+                             call clear_paddle2
+                             mov  ax , paddle_velocity
+    ; Boundary check for the left edge of the screen
+                             mov  bx, paddle_two_x
+                             sub  bx, ax
+                             cmp  bx, 0                       ; Ensure it doesn't go below 0
+                             jl   exit_paddle_movement        ; If beyond left boundary, skip update
+                             sub  paddle_two_x , ax
+
+    exit_paddle_movement:    
+                             ret
+move_paddles endp
+
+
+clear_paddle1 proc far
+
+                             mov  cx , paddle_one_x
+                             mov  dx , paddle_one_y
+    clear_paddle1_horizontal:
+                             mov  ah, 0ch                     ; pixel color interrupt
+                             mov  al, 00h                     ;pixel color white
+                             mov  bh,0h
+                             int  10h                         ;call interrupt
+                             inc  cx                          ; move to left
+                             mov  ax , cx
+                             sub  ax , paddle_one_x
+                             cmp  ax , paddle_width
+                             jl   clear_paddle1_horizontal
+
+                             mov  cx , paddle_one_x
+                             inc  dx
+                             mov  ax , dx
+                             sub  ax , paddle_one_y
+                             cmp  ax , paddle_height
+                             jl   clear_paddle1_horizontal
+
+                             ret
+clear_paddle1 endp
+
+clear_paddle2 proc far
+
+                             mov  cx , paddle_two_x
+                             mov  dx , paddle_two_y
+
+    clear_paddle2_horizontal:
+                             mov  ah, 0ch                     ; pixel color interrupt
+                             mov  al, 00h                     ;pixel color white
+                             mov  bh,0h
+                             int  10h                         ;call interrupt
+                             inc  cx                          ; move to left
+                             mov  ax , cx
+                             sub  ax , paddle_two_x
+                             cmp  ax , paddle_width
+                             jl   clear_paddle2_horizontal
+
+                             mov  cx , paddle_two_x
+                             inc  dx
+
+                             mov  ax , dx
+                             sub  ax , paddle_two_y
+                             cmp  ax , paddle_height
+                             jl   clear_paddle2_horizontal
+
+
+
+                             ret
+clear_paddle2 endp
+
+end main
