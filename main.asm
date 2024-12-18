f35988a8fcb38ad66c281d7ce1f7d5c4d694e81b
Hussein <shsmohmed4@gmail.com>
2024-12-18 14:25:59 +0200

    Implement winning condition check and display winning message

--- main.asm
+++ main.asm
@@ -2,79 +2,86 @@
 .stack 100h
 
 .DATA
-AUX_TIME DB 0
+    AUX_TIME DB 0
 .code
 
 INIT_APP PROC NEAR
     ; Set video mode 13h (320x200, 256 colors)
-    mov ax, 0013h ; Set video mode 13h
-    int 10h       ; Call BIOS interrupt
+                    mov   ax, 0013h                    ; Set video mode 13h
+                    int   10h                          ; Call BIOS interrupt
 
     ; Clear the screen (fill video memory with 0)
-    mov       ax, 0A000h ; Video memory segment address
-    mov       es, ax     ; ES = video memory
-    xor       di, di     ; Starting offset in video memory
-    mov       cx, 32000  ; 320x200 pixels, 1 byte per pixel
-    mov       al, 00h    ; Black color (0)
-    rep stosb            ; Clear the screen
+                    mov   ax, 0A000h                   ; Video memory segment address
+                    mov   es, ax                       ; ES = video memory
+                    xor   di, di                       ; Starting offset in video memory
+                    mov   cx, 32000                    ; 320x200 pixels, 1 byte per pixel
+                    mov   al, 00h                      ; Black color (0)
+                    rep   stosb                        ; Clear the screen
     
-    RET
-ENDP INIT_APP
+                    RET
+                    ENDP  INIT_APP
 
-    EXTRN DRAWBLOCKS:FAR
-    EXTRN DRAW_BALL:FAR
-    EXTRN MOVE_BALL_BY_VELOCITY:FAR
-    EXTRN DELETE_BALL:FAR
-    EXTRN move_paddles:FAR
-    EXTRN draw_paddles:FAR
+                    EXTRN DRAWBLOCKS:FAR
+                    EXTRN DRAW_BALL:FAR
+                    EXTRN MOVE_BALL_BY_VELOCITY:FAR
+                    EXTRN DELETE_BALL:FAR
+                    EXTRN move_paddles:FAR
+                    EXTRN draw_paddles:FAR
+                    EXTRN CHECK_SCREEN_PIXELS:FAR
+                    EXTRN DISPLAY_WIN_MESSAGE:FAR
 
 main PROC
-    MOV   AX, @DATA
-    MOV   DS, AX
+                    MOV   AX, @DATA
+                    MOV   DS, AX
 
-    CALL  INIT_APP
-    CALL  DRAWBLOCKS
+                    CALL  INIT_APP
+                    CALL  DRAWBLOCKS
 
 
-; GET TIME CH Hours, CL Minutes, DH Seconds, DL Hundreths of a second
-CHECK_TIME:
+    ; GET TIME CH Hours, CL Minutes, DH Seconds, DL Hundreths of a second
+    CHECK_TIME:     
 
     ; PADDLE STUFF
-    call  draw_paddles
-    mov  ah , 01h
-    int  16h
-    jz   NO_INPUT_ACTION
+                    call  draw_paddles
+                    mov   ah , 01h
+                    int   16h
+                    jz    NO_INPUT_ACTION
 
-    CMP   AL, 27                       ; Check if key is ESC
-    JE    exit                         ; If ESC, exit program
+                    CMP   AL, 27                       ; Check if key is ESC
+                    JE    exit                         ; If ESC, exit program
 
-    call  move_paddles
+                    call  move_paddles
 
     NO_INPUT_ACTION:
-    MOV   AH, 2CH
-    INT   21H
+                    MOV   AH, 2CH
+                    INT   21H
 
-    CMP   DL, AUX_TIME
-    JE    CHECK_TIME
+                    CMP   DL, AUX_TIME
+                    JE    CHECK_TIME
 
-    MOV   AUX_TIME, DL
+                    MOV   AUX_TIME, DL
 
 
-   ; BALL MOVEMENT
-    CALL  DELETE_BALL
-    CALL  MOVE_BALL_BY_VELOCITY
-    CALL  DRAW_BALL
+    ; BALL MOVEMENT
+                    CALL  DELETE_BALL
+                    CALL  MOVE_BALL_BY_VELOCITY
+                    CALL  DRAW_BALL
 
-    JMP   CHECK_TIME
+    ; chexk win condition
+                    call  CHECK_SCREEN_PIXELS
+                    cmp   al , 1
+                    je    GAME_WON
+                    JMP   CHECK_TIME
+    GAME_WON:       
+    call DISPLAY_WIN_MESSAGE
+    exit:           
+    ; Clear screen
+                    MOV   AH, 0
+                    MOV   AL, 3
+                    INT   10H
 
-exit:               
-; Clear screen
-    MOV   AH, 0
-    MOV   AL, 3
-    INT   10H
-
-; Exit program
-    MOV   AH, 4CH
-    INT   21H
+    ; Exit program
+                    MOV   AH, 4CH
+                    INT   21H
 main ENDP
 END  main
---
+++ win.asm
@@ -0,0 +1,40 @@
+.model small    
+.data
+NUMBER_OF_ROWS DW 3
+
+.stack 100h
+.code
+PUBLIC CHECK_SCREEN_PIXELS
+CHECK_SCREEN_PIXELS PROC far
+    ; Video memory starts at segment A000h
+    MOV     AX, 0A000h      ; Video memory segment
+    MOV     ES, AX          ; ES = A000h
+    XOR     DI, DI          ; Start from offset 0
+
+    ; Calculate the total number of pixels to check
+    MOV     AX, NUMBER_OF_ROWS ; Load the number of rows
+    MOV     BX, 320           ; Number of pixels per row
+    MUL     BX                ; AX = NUMBER_OF_ROWS * 320
+    MOV     CX, AX            ; CX = Total pixels to check
+
+CHECK_PIXEL:
+    MOV     AL, ES:[DI]       ; Load pixel color directly from video memory
+    CMP     AL, 0             ; Is the pixel black (0)?
+    JE      NEXT_PIXEL        ; If yes, continue to next pixel
+    CMP     AL, 15            ; Is the pixel white (15)?
+    JE      NEXT_PIXEL        ; If yes, continue to next pixel
+
+    ; If pixel is neither black nor white, exit with no win
+    MOV     AL, 0             ; Set AL to indicate not all pixels are black or white
+    RET
+
+NEXT_PIXEL:
+    INC     DI                ; Move to next pixel
+    LOOP    CHECK_PIXEL       ; Continue checking all pixels
+
+    ; If all pixels are black or white, return success
+    MOV     AL, 1             ; Set AL to indicate win condition
+    RET
+CHECK_SCREEN_PIXELS ENDP
+
+END
